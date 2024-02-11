0e9c39ef0973561f43c965dbfa2e5466597d8706
Rolando Murillo <rolandomuagui1629gmail.com>
2024-02-11 00:23:24 -0600

    Cambios en index, estilod y funcionalidad js

--- script/encriptation.js
+++ script/encriptation.js
@@ -1,1 +1,9 @@
+let textAreaIn = document.getElementById('textAreaInput');
 
+textAreaIn.style.cssText = `height: ${textAreaIn.scrollHeight}px; overflow-y: hidden`;
+
+
+textAreaIn.addEventListener("input", function(){
+    this.style.height = "auto";
+    this.style.height = `${this.scrollHeight}px`;
+});
