37493219e38a14d533e9791295c22bca96e3ef67
Rolando Murillo <rolandomuagui1629gmail.com>
2024-03-03 19:16:51 -0600

    Cambios generales en scripts y diseño

--- script/encriptation.js
+++ script/encriptation.js
@@ -1,9 +1,35 @@
-let textAreaIn = document.getElementById('textAreaInput');
+let textAreaInp = document.getElementById("textAreaInput");
+let textAreaOut = document.getElementById("textAreaOutput");
 
-textAreaIn.style.cssText = `height: ${textAreaIn.scrollHeight}px; overflow-y: hidden`;
+let buttonEncriptar = document.getElementById("buttonEncriptar");
 
+let containerText =  document.getElementById('textosOutput');
 
-textAreaIn.addEventListener("input", function(){
-    this.style.height = "auto";
-    this.style.height = `${this.scrollHeight}px`;
+let buttonCopy =  document.getElementById('buttonCopiar');
+
+textAreaInp.style.cssText = `height: ${textAreaInp.scrollHeight}px; overflow-y: hidden`;
+textAreaInp.addEventListener("input", function () {
+  this.style.height = "auto";
+  this.style.height = `${this.scrollHeight}px`;
 });
+
+textAreaOut.style.cssText = `height: ${textAreaOut.scrollHeight}px; overflow-y: hidden`;
+textAreaOut.addEventListener("input", function () {
+  this.style.height = "auto";
+  this.style.height = `${this.scrollHeight}px`;
+});
+
+function pasarValor() {
+  let textoInp = textAreaInp.value;
+
+  if (textAreaInp.scrollHeight > 0) {
+    textAreaOut.style.height = `${textAreaInp.scrollHeight}px`;
+    containerText.style.display = "none";
+    textAreaOut.innerText = textoInp;
+    buttonCopy.style.display = "flex";
+  }else{
+    textAreaOut.style.height = "initial";
+    containerText.style.display = "initial";
+    buttonCopy.style.display = "initial";
+  }
+}
